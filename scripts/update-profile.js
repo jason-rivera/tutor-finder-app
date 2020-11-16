3ead6161a5f62408573da0676b51ca2594926a92
Jason Rivera <jp_rivera_@hotmail.com>
2020-11-15 16:28:00 -0800

    updated profile page(can now upload profile picture via IMG URL, updated search page

--- scripts/update-profile.js
+++ scripts/update-profile.js
@@ -1,22 +1,37 @@
 function updateProfile() {  
+  $(document).ready(function () {
+    $("#update-btn").click(function (){
+      firebase.auth().onAuthStateChanged(function (user) {
+        if (user){
+          db.collection("Users").doc(user.uid).update({
+            name: document.getElementById("name-text").value,
+            email: document.getElementById("email-text").value,
+            birthYear: document.getElementById("birth-year-text").value,
+            location: document.getElementById("location-text").value,
+            photoURL: document.getElementById("picture-text").value,
+          })
+        }
+      })
+    });
+  });
+
+
+
+
 	$('#update-profile').submit(function () {
 		return false;
   });
 }
   
-$(document).ready(function () {
-  $("#update-btn").click(function (){
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user){
-        db.collection("Users").doc(user.uid).update({
-          name: document.getElementById("name-text").value,
-          email: document.getElementById("email-text").value,
-          birthYear: document.getElementById("birth-year-text").value,
-          location: document.getElementById("location-text").value,
-        })
-      }
-    })
-  });
-});
+
+
 
 updateProfile();
+
+
+
+
+// var input = document.getElementById('myInput');
+
+// if(input.value.length == 0)
+//     input.value = "Empty";

5a30ea5144edac5bcc15d1a6a4e09021fdc5e742
Connor Young <connor-young@outlook.com>
2020-11-20 09:25:22 -0800

    refreshed profile.html

--- scripts/update-profile.js
+++ scripts/update-profile.js
@@ -1,25 +1,27 @@
 function updateProfile() {  
-  $(document).ready(function () {
-    
-    $("#update-btn").click(function (){
-      firebase.auth().onAuthStateChanged(function (user) {
-        if (user){
-          db.collection("Users").doc(user.uid).update({
-            name: document.getElementById("name-text").value,
-            email: document.getElementById("email-text").value,
-            birthYear: document.getElementById("birth-year-text").value,
-            location: document.getElementById("location-text").value,
-            photoURL: document.getElementById("picture-text").value,
-            
-          })
-        }
-      })
-    });
-  });
+
+	$(document).ready(function () {
+		$("#update-btn").click(function (){
+			firebase.auth().onAuthStateChanged(function (user) {
+				if (user){
+					db.collection("Users").doc(user.uid).set({
+						name: document.getElementById("name-text").value,
+						email: document.getElementById("email-text").value,
+						birthYear: document.getElementById("birth-year-text").value,
+						location: document.getElementById("location-text").value,
+						photoURL: document.getElementById("picture-text").value,
+					})
+					.then(function() {
+						window.location.href = "profile.html";
+					})
+				}
+			})
+		});
+	});
 
 	$('#update-profile').submit(function () {
 		return false;
-  });
+	});
 }
   
 updateProfile();

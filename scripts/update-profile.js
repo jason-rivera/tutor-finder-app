fb0b8ed3d12457ad775630d7bb859b19be76f788
Connor Young <connor-young@outlook.com>
2020-11-20 09:46:02 -0800

    refreshed profile.html

--- scripts/update-profile.js
+++ scripts/update-profile.js
@@ -1,10 +1,11 @@
 function updateProfile() {  
 
 	$(document).ready(function () {
+
 		$("#update-btn").click(function (){
 			firebase.auth().onAuthStateChanged(function (user) {
 				if (user){
-					db.collection("Users").doc(user.uid).set({
+					db.collection("Users").doc(user.uid).update({
 						name: document.getElementById("name-text").value,
 						email: document.getElementById("email-text").value,
 						birthYear: document.getElementById("birth-year-text").value,

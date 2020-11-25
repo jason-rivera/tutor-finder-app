696f3ac93c80b071284e0aa1a3be85952dba90aa
Connor Young <connor-young@outlook.com>
2020-11-24 19:03:38 -0800

    tutor-form filled if user's already a tutor

--- scripts/write-tutor.js
+++ scripts/write-tutor.js
@@ -1,57 +1,83 @@
-function writeTutor() {  
+function submitButton() {  
 	$('#tutor-form').submit(function () { //makes it so page doesn't refresh on submit
 		return false;
 	});
-  
 	$(document).ready(function () {
 		$("#submit-button").click(function (){
-			firebase.auth().onAuthStateChanged(function (user) {
-				if (user){
-					db.collection("Tutors").doc(user.uid).get()
-					.then(function(docSnapshot) {
-						if(docSnapshot.exists) {
-							updateTutorFields(user);
-							setCheckedSubjects(user);
-						} else {
-							setTutorFields(user);
-							setCheckedSubjects(user);
-						}
-					});
-				} else {
-					console.log("no user signed in");
-				}
-				writeTimeslot();
-				
-			})
+			writeTutor();
 		});
+		//
 	});
 }
 
-function setCheckedSubjects(user) {
-	$('.form-check-input').each(function() {
+function writeTutor() {
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user){
+			let tutorRef = db.collection("Tutors").doc(user.uid);
+			tutorRef.get()
+			.then(function(docSnapshot) {
+				if(docSnapshot.exists) {
+					updateTutorFields(user, tutorRef);
+				} else {
+					setTutorFields(user, tutorRef);
+				}
+				writeTimeslot();
+				setCheckedSubjects(user, tutorRef);
+			});
+		} else {
+			console.log("no user signed in");
+		}
+	})
+}
+
+function fillForm() {
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user){
+			let tutorRef = db.collection("Tutors").doc(user.uid);
+			tutorRef.get()
+			.then(function(docSnapshot) {
+				if(docSnapshot.exists) {
+					$("#tutor-description").val(docSnapshot.data().description);
+					$("#rate").val(docSnapshot.data().rate);
+					for (i = 0; i < docSnapshot.data().subjects.length; i++) {
+						$('#' + docSnapshot.data().subjects[i]).prop("checked", true );
+					}
+				}
+			});
+		} else {
+			console.log("no user signed in");
+			window.location.href = "index.html";
+		}
+	});
+}
+
+function setCheckedSubjects(user, tutorRef) {
+	$('.subject-check-input').each(function() {
 		if(this.checked) {
-			db.collection("Tutors").doc(user.uid).update({
+			tutorRef.update({
 				subjects: firebase.firestore.FieldValue.arrayUnion(this.id)
 			})
 		}
 	})
 }
 
-function updateTutorFields(user) {
+function updateTutorFields(user, tutorRef) {
 	console.log(user.uid + " has been updated.");
-	db.collection("Tutors").doc(user.uid).update({
+	tutorRef.update({
 		description: document.getElementById("tutor-description").value,
 		rate: parseFloat(document.getElementById("rate").value),
-		subjects: [],
+		subjects: [] //don't worry about finding unchecked subjects, just reset the array
+		//reviews and rating get updated when a review is submitted
 	});
 }
 
-function setTutorFields(user) {
+function setTutorFields(user, tutorRef) {
 	console.log(user.uid + "is now a tutor.");
-	db.collection("Tutors").doc(user.uid).set({
+	tutorRef.set({
 		description: document.getElementById("tutor-description").value,
 		rate: parseFloat(document.getElementById("rate").value),
 		rating: 0,
+		reviews: 0,
 		subjects: [],
 	})
 }
@@ -69,4 +95,5 @@ function writeTimeslot() {
 	});
 }
 
-writeTutor();
+fillForm();
+submitButton()

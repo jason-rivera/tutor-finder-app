19362604096cb831b0420135c75fac4b46f354d5
Connor Young <connor-young@outlook.com>
2020-12-01 09:56:43 -0800

    profile pic fixed

--- scripts/get-profile.js
+++ scripts/get-profile.js
@@ -13,7 +13,7 @@ function getUser() {
           console.log('Birthdate: ' + birthYear);
           console.log('Location: ' + location);
           console.log('Your photo is: ' + pic);
-          document.getElementById("your-picture").innerHTML = '<center> <img src="' + pic + '" height="150" width="150" alt="Your photo" style="border: 4px solid white; border-radius: 100px;"></center><br>';
+          document.getElementById("your-picture").innerHTML = '<center> <img id="profile-pic" src="' + pic + '"></center><br>';
           document.getElementById("your-name").innerText = name;
           document.getElementById("your-email").innerText = email;
           document.getElementById("your-year").innerHTML = birthYear;

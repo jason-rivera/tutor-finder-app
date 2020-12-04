b8ff08d31dabc19b3e7d3e7d6286e024b8921e80
Connor Young <connor-young@outlook.com>
2020-12-03 16:10:53 -0800

    nav bar

--- scripts/navbar-common.js
+++ scripts/navbar-common.js
@@ -3,33 +3,28 @@ function navbarCommonAdd() {
     $("body").prepend('<navbar id="navBarCommonBar" class="navbar navbar-expand-lg navbar-dark bg-dark">');
     $("#navBarCommonBar").append('<a class="navbar-brand" href=""></a>');
     $("#navBarCommonBar a").text(navBarCommonName.name);
-    $("#navBarCommonBar").append('<button class="navbar-toggler" id="navBarButton" type="button" data-toggle="collapse" data-target="#navbarToggler">');
-    $("#navBarButton").append('<span class="navbar-toggler-icon">');
-    $("#navBarCommonBar").append('<div class="collapse navbar-collapse" id="navbarToggler">');
-    $("#navbarToggler").append('<ul id="burgerMenu" class="navbar-nav ml-auto">');
-	$("#navBarburgerMenu").append('<li class="nav-item" id="searchNavButton">');
-	$("#navBarburgerMenu").append('<li class="nav-item" id="reviewsNavButton">');
-	$("#burgerMenu").append('<li class="nav-item" id="homeNavButton">');
-    $("#burgerMenu").append('<li class="nav-item" id="loginNavButton">');
-    $("#homeNavButton").append('<a class="nav-link" href="index.html">Home</a>');
-    $("#homeNavButton").append('<a class="nav-link" href="profile.html">Profile</a>');
-	$("#homeNavButton").append('<a class="nav-link" href="searchV2.html">Search</a>');
-	$("#homeNavButton").append('<a class="nav-link" href="previous-sessions.html">Previous Sessions</a>');
-	$("#homeNavButton").append('<a class="nav-link" href="tutor-form.html">Tutor Form</a>');
-	$("#homeNavButton").append('<a class="nav-link" href="reviews.html">Reviews</a>');
-	// Login/logout link below
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user){
-			$("#loginNavButton").append('<a class="nav-link">Log Out</a>');
+
+	firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+		    $("#navBarCommonBar").append('<button class="navbar-toggler" id="navBarButton" type="button" data-toggle="collapse" data-target="#navbarToggler">');
+			$("#navBarButton").append('<span class="navbar-toggler-icon">');
+			$("#navBarCommonBar").append('<div class="collapse navbar-collapse" id="navbarToggler">');
+			$("#navbarToggler").append('<ul id="burgerMenu" class="navbar-nav ml-auto">'); 
+			$("#navBarburgerMenu").append('<li class="nav-item" id="searchNavButton">');
+			$("#navBarburgerMenu").append('<li class="nav-item" id="reviewsNavButton">');
+			$("#burgerMenu").append('<li class="nav-item" id="homeNavButton">');
+			$("#burgerMenu").append('<li class="nav-item" id="loginNavButton">');
+			$("#homeNavButton").append('<a class="nav-link" href="index.html">Home</a>');
+			$("#homeNavButton").append('<a class="nav-link" href="profile.html">Profile</a>');
+			$("#homeNavButton").append('<a class="nav-link" href="searchV2.html">Search</a>');
+			$("#homeNavButton").append('<a class="nav-link" href="previous-sessions.html">Previous Sessions</a>');
+			$("#homeNavButton").append('<a class="nav-link" href="tutor-form.html">Tutor Form</a>');
+		    $("#loginNavButton").append('<a class="nav-link">Log Out</a>');
 			$("#loginNavButton").click(function() {
 				logout();
-			})
-        }
-        else {
-            $("#homeNavButton").append('<a class="nav-link" href="login.html">Log In</a>');
-        }
-    })
-				
+			});
+		}
+	});			
 }
 
 function logout(){ //needed for logout functionality on logout button
@@ -44,28 +39,3 @@ function logout(){ //needed for logout functionality on logout button
 }
 
 navbarCommonAdd()
-
-/* Old Code
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="">Home</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarToggler">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="search.html">Search</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="review-tutor.html">Reviews</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="login.html">Login</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-*/

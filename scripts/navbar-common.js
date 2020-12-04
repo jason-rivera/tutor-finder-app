c38c355f9582bc11c72d63de8d76de19e225f8b6
Sanderson162 <sanderson162@my.bcit.ca>
2020-12-03 19:46:00 -0800

    Navbar

--- scripts/navbar-common.js
+++ scripts/navbar-common.js
@@ -1,9 +1,6 @@
-/* this code is badly formatted however it makes the textblock a little clearer */
-
 function addNavbarCommon(currentPage) {
 	let fileName = String(currentPage.split("/").slice(-1)); 
-	//let fileName = String(location.href.split("/").slice(-1)); 
-	//alert(fileName)
+	
 	let pagenames = new Map([
 		['index.html', "Home"],
 		['profile.html', "Profile"],
@@ -13,11 +10,17 @@ function addNavbarCommon(currentPage) {
 		['reviews.html', "Reviews"],
 		['login.html', "Login"]
 	])
-	console.log(fileName)
+
+	if (!pagenames.has(fileName)) {
+		fileName = 'index.html'
+		console.log("pagename does not exist, using default");
+	}
+
+	console.log("filename: " + fileName)
 	console.log(pagenames.get(fileName))
 
 	$("body").prepend(
-		'<nav id="navBarCommonBar" class="navbar navbar-expand-lg navbar-dark bg-dark">'
+		'<nav id="navBarCommonBar" class="navbar navbar-expand-xl navbar-dark bg-dark">'
 		+	'	<a class="navbar-brand" href="#">' + pagenames.get(fileName) + '</a>'
 		+	'	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">'
 		+	'		<span class="navbar-toggler-icon"></span>'
@@ -25,31 +28,29 @@ function addNavbarCommon(currentPage) {
 		+	'	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">'
 		+	'		<div class="navbar-nav">'
 		+	'			<a id="navButton_Home" class="nav-item nav-link" href="index.html">Home</a>'
-		+	'			<a id="navButton_Profile" class="nav-item nav-link" href="profile.html" disabled>Profile</a>'
 		+	'			<a id="navButton_Search" class="nav-item nav-link" href="searchV2.html">Search</a>'
-		+	'			<a id="navButton_Sessions" class="nav-item nav-link" href="sessions.html" disabled>Sessions</a>'
-		+	'			<a id="navButton_Tutor" class="nav-item nav-link" href="tutor-form.html" disabled>Tutor Form</a>'
+		+	'			<a id="navButton_Sessions" class="nav-item nav-link" href="sessions.html">Sessions</a>'
+		+	'			<a id="navButton_Tutor" class="nav-item nav-link" href="tutor-form.html">Tutor Form</a>'
+		+	'			<a id="navButton_Profile" class="nav-item nav-link" href="profile.html">Profile</a>'
 		+	'			<a id="navButton_Login"class="nav-item nav-link" href="login.html">Log In</a>'
 		+	'		</div>'
 		+	'	</div>'
 		+	'</nav>'
 		);
-
+		
 		$('a[href="'+fileName+'"').addClass("active")
 
-		/*
-		+	'			<a id="" class="nav-item nav-link" href="#"></a>'
-		+	'			<a id="" class="nav-item nav-link" href="#"></a>'
-		*/
-
+		$('#navButton_Profile').hide()
+		$('#navButton_Sessions').hide()
+		$('#navButton_Tutor').hide()
 
 		firebase.auth().onAuthStateChanged(function (user) {
 			if (user) {
-				$('#navButton_Profile').prop('disabled', false);
-				$('#navButton_Sessions').prop('disabled', false);
-				$('#navButton_Tutor').prop('disabled', false);
-				$("#loginNavButton").text("Log Out")
-				$("#loginNavButton").click(function() {
+				$('#navButton_Profile').show()
+				$('#navButton_Sessions').show()
+				$('#navButton_Tutor').show()
+				$("#navButton_Login").text("Log Out")
+				$("#navButton_Login").click(function() {
 					logout();
 				});
 			}
@@ -102,4 +103,4 @@ function logout(){ //needed for logout functionality on logout button
 
 }
 
-navbarCommonAdd()
+//navbarCommonAdd()
